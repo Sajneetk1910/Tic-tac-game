d2f939ce39c7f3fa4f6c009d6ecedddb1eb7734f
Sajneetk1910 <your-email@example.com>
2025-03-27 12:04:49 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Game from "./Components/Game";
+// import Board from "./Components/Board";
+function App() {
+  return (
+    <>
+      {/* <Board/>   */}
+      <Game/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Game.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export default function Game() {
+    const[xIsNext,setXIsNext]=useState(true);
+    const[history,setHistory]=useState([Array(9).fill(null)]);
+    const currentSquares=history[history.length-1];
+
+    function handlePlay(nextSquares){
+            
+    }
+
+
+
+
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol></ol>
+      </div>
+    </div>
+  );
+}
